1e0fe46931b3e35d6476a2e19103f6b0ecc05e2d
kevan Jahanshahi <kevan@jahia.com>
2024-02-21 09:16:22 +0100

    BACKLOG-22357: Introduce new tooling to investigate import-package and export-package in the system (#61)
    * BACKLOG-22357: Introduce Import-Package checker tool
    
    * BACKLOG-22357: Introduce Import-Package checker tool
    
    * BACKLOG-22357: Introduce GraphQL API for tooling, and new API to find for export-package
    
    * BACKLOG-22357: java doc
    
    * BACKLOG-22357: fix GQL naming
    
    * BACKLOG-22357: code review fixes

---
+++ src/main/java/org/jahia/modules/tools/gql/admin/osgi/FindExportPackage.java
@@ -0,0 +1,35 @@
+package org.jahia.modules.tools.gql.admin.osgi;
+
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@GraphQLName("FindExportPackage")
+@GraphQLDescription("Result of the export package inspector operation.")
+public class FindExportPackage {
+    int totalCount = 0;
+
+    List<ExportPackages> results = new ArrayList<>();
+
+    public void add(ExportPackages result) {
+        results.add(result);
+        totalCount += result.getMatchingPackages().size();
+    }
+
+    @GraphQLField
+    @GraphQLName("totalCount")
+    @GraphQLDescription("Total number of matching export packages.")
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    @GraphQLField
+    @GraphQLName("packages")
+    @GraphQLDescription("List of matching packages")
+    public List<ExportPackages> getExportPackages() {
+        return results;
+    }
+}
---
+++ src/main/java/org/jahia/modules/tools/gql/admin/osgi/OSGIPackageHeaderChecker.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2002-2022 Jahia Solutions Group SA. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jahia.modules.tools.gql.admin.osgi;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.felix.utils.manifest.Clause;
+import org.apache.felix.utils.manifest.Parser;
+import org.jahia.osgi.FrameworkService;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Version;
+import org.osgi.framework.VersionRange;
+
+import java.util.*;
+
+/**
+ * Utility class for the OSGI Import-Package, Export-Package checker.
+ *
+ * @author jkevan
+ */
+public class OSGIPackageHeaderChecker {
+
+    /**
+     * Perform the OSGI Import-Package checker. This method will check all bundles in the OSGI
+     * framework and return a list of matching import packages.
+     *
+     * @param regex The regular expression to match against the import package name.
+     * @param matchVersion The version to match against the import package version.
+     * @param matchVersionRangeMissing If true, will only return import packages that are missing a version range.
+     *
+     * @return The result of the import package checker.
+     */
+    public static FindImportPackage findImportPackages(String regex, String matchVersion, boolean matchVersionRangeMissing) {
+        FindImportPackage results = new FindImportPackage();
+
+        Bundle[] bundles = FrameworkService.getBundleContext().getBundles();
+        for (Bundle bundle : bundles) {
+            BundleWithImportPackages entry = new BundleWithImportPackages(bundle);
+            String importPackageHeader = bundle.getHeaders().get("Import-Package");
+            if (importPackageHeader != null) {
+                Clause[] importedPackages = Parser.parseHeader(importPackageHeader);
+                for (Clause importedPackageClause : importedPackages) {
+                    if (StringUtils.isEmpty(regex) || importedPackageClause.getName().matches(regex)) {
+                        String versionStr = importedPackageClause.getAttribute("version");
+
+                        // Version missing check
+                        if (matchVersionRangeMissing) {
+                            if (versionStr == null) {
+                                entry.addMatchingImportedPackage(importedPackageClause.toString());
+                            }
+                            continue;
+                        }
+
+                        // Version match check
+                        if (StringUtils.isNotEmpty(matchVersion)) {
+                            if (versionStr == null) {
+                                entry.addMatchingImportedPackage(importedPackageClause.toString());
+                            } else {
+                                Version osgiVersion = Version.parseVersion(matchVersion);
+                                VersionRange osgiVersionRange = new VersionRange(versionStr);
+                                if (osgiVersionRange.includes(osgiVersion)) {
+                                    entry.addMatchingImportedPackage(importedPackageClause.toString());
+                                }
+                            }
+                            continue;
+                        }
+
+                        // No condition check
+                        entry.addMatchingImportedPackage(importedPackageClause.toString());
+                    }
+                }
+            }
+            if (!entry.getMatchingImportedPackage().isEmpty()) {
+                results.add(entry);
+            }
+        }
+        return results;
+    }
+
+    /**
+     * Perform the OSGI Export-Package checker. This method will check all bundles in the OSGI
+     * framework and return a list of matching export packages.
+     *
+     * @param regex The regular expression to match against the export package name.
+     * @param duplicates If true, will only return export packages found multiple times.
+     *
+     * @return The result of the export package checker.
+     */
+    public static FindExportPackage findExportPackages(String regex, boolean duplicates) {
+        // Collect export packages
+        Map<String, List<BundleWithExportPackage>> packages = new HashMap<>();
+        Bundle[] bundles = FrameworkService.getBundleContext().getBundles();
+        for (Bundle bundle : bundles) {
+            String exportPackageHeader = bundle.getHeaders().get("Export-Package");
+            if (exportPackageHeader != null) {
+                Clause[] exportPackages = Parser.parseHeader(exportPackageHeader);
+                for (Clause exportPackage : exportPackages) {
+                    if (StringUtils.isEmpty(regex) || exportPackage.getName().matches(regex)) {
+                        BundleWithExportPackage resultEntry = new BundleWithExportPackage(exportPackage.toString(), bundle);
+                        if (packages.containsKey(exportPackage.getName())) {
+                            packages.get(exportPackage.getName()).add(resultEntry);
+                        } else {
+                            packages.put(exportPackage.getName(), new ArrayList<>(Collections.singletonList(resultEntry)));
+                        }
+                    }
+                }
+            }
+        }
+
+        // filter duplicates if needed
+        FindExportPackage results = new FindExportPackage();
+        for (Map.Entry<String, List<BundleWithExportPackage>> packageEntries : packages.entrySet()) {
+            if (!duplicates || packageEntries.getValue().size() > 1) {
+                ExportPackages result = new ExportPackages(packageEntries.getKey());
+                result.setExportPackages(packageEntries.getValue());
+                results.add(result);
+            }
+        }
+        return results;
+    }
+}
