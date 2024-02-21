402c0a157dcb9ce821451137c1b295459f88c900
kevan Jahanshahi <kevan@jahia.com>
2024-02-21 09:16:22 +0100

    BACKLOG-22357: Introduce new tooling to investigate import-package and export-package in the system (#61)
    * BACKLOG-22357: Introduce Import-Package checker tool
    
    * BACKLOG-22357: Introduce Import-Package checker tool
    
    * BACKLOG-22357: Introduce GraphQL API for tooling, and new API to find for export-package
    
    * BACKLOG-22357: java doc
    
    * BACKLOG-22357: fix GQL naming
    
    * BACKLOG-22357: code review fixes

---
+++ src/main/java/org/jahia/modules/tools/gql/admin/ToolsGraphQL.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2002-2022 Jahia Solutions Group SA. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jahia.modules.tools.gql.admin;
+
+import graphql.annotations.annotationTypes.*;
+import org.jahia.modules.graphql.provider.dxm.util.GqlUtils;
+import org.jahia.modules.tools.gql.admin.osgi.FindExportPackage;
+import org.jahia.modules.tools.gql.admin.osgi.OSGIPackageHeaderChecker;
+import org.jahia.modules.tools.gql.admin.osgi.FindImportPackage;
+
+
+@GraphQLName("AdminTools")
+@GraphQLDescription("Jahia admin tools")
+public class ToolsGraphQL {
+    @GraphQLField
+    @GraphQLDescription("Will return all the import packages matching the given parameters.")
+    public FindImportPackage findMatchingImportPackages(
+            @GraphQLName("RegExp") @GraphQLDescription("will return only import-package matching the RegExp") String regExp,
+            @GraphQLName("version") @GraphQLDescription("will return only import-package matching the given version") String version,
+            @GraphQLName("versionMissing") @GraphQLDescription("will return only import-package with no version range limitations") @GraphQLDefaultValue(GqlUtils.SupplierFalse.class) boolean versionMissing
+    ) {
+        return OSGIPackageHeaderChecker.findImportPackages(regExp, version, versionMissing);
+    }
+
+    @GraphQLField
+    @GraphQLDescription("Will return all the duplicate export packages and the bundles that export them")
+    public FindExportPackage findMatchingExportPackages(
+            @GraphQLName("RegExp") @GraphQLDescription("will return only export-package matching the RegExp") String regExp,
+            @GraphQLName("duplicates") @GraphQLDescription("will return only export-package found multiple times for a same package name") @GraphQLDefaultValue(GqlUtils.SupplierFalse.class) boolean duplicates
+    ) {
+        return OSGIPackageHeaderChecker.findExportPackages(regExp, duplicates);
+    }
+}
---
+++ src/main/java/org/jahia/modules/tools/gql/admin/osgi/ExportPackages.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2002-2022 Jahia Solutions Group SA. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jahia.modules.tools.gql.admin.osgi;
+
+import graphql.annotations.annotationTypes.GraphQLDescription;
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLName;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@GraphQLName("ExportPackages")
+@GraphQLDescription("Result of the export package inspector operation.")
+public class ExportPackages {
+    String packageName;
+
+    List<BundleWithExportPackage> exportPackages;
+
+    public ExportPackages(String packageName) {
+        this.packageName = packageName;
+    }
+
+    @GraphQLField
+    @GraphQLName("packageName")
+    @GraphQLDescription("the package name.")
+    public String getPackageName() {
+        return packageName;
+    }
+
+    @GraphQLField
+    @GraphQLName("matchingExportPackagesDetailed")
+    @GraphQLDescription("The bundles that export the package.")
+    public List<BundleWithExportPackage> getMatchingPackageDetailed() {
+        return exportPackages;
+    }
+
+    @GraphQLField
+    @GraphQLName("matchingExportPackages")
+    @GraphQLDescription("Flat list of exported packages")
+    public List<String> getMatchingPackages() {
+        return exportPackages.stream().map(
+                (BundleWithExportPackage entry) -> entry.getPackage() + " EXPOSED BY " + entry.getBundleDisplayName()
+        ).collect(Collectors.toList());
+    }
+
+    public void setExportPackages(List<BundleWithExportPackage> entries) {
+        this.exportPackages = entries;
+    }
+}
