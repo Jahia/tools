1994040e8584ddf99d020f6f4ee34f54777c9302
david.griffon <dgriffon@jahia.com>
2025-04-17 19:33:09 +0200

    chore(sonar): handle sonar errors

--- src/javascript/datatable.js
+++ src/javascript/datatable.js
@@ -7,7 +7,8 @@ import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-table="dataTable"]')
         .forEach(dataTable => {
-            // eslint-disable-next-line no-unused-vars
-            const table = new Datatable(dataTable);
+            // Instanciate a datatable (ignore sonar warings)
+            // eslint-disable-next-line no-new
+            new Datatable(dataTable); // NOSONAR
         });
 });
