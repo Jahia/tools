61f7e7a1972b3409bbdd5304d6537bcae5fa595f
Baptiste Grimaud <bgrimaud@jahia.com>
2025-06-26 17:20:20 +0200

    feat(definitions): improve search accuracy

--- src/javascript/datatable.js
+++ src/javascript/datatable.js
@@ -7,11 +7,36 @@ import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-table="dataTable"]')
         .forEach(dataTable => {
-            // Instanciate a datatable (ignore sonar warings)
+            // Instantiate a datatable (ignore sonar warnings)
+            // eslint-disable-next-line no-new
+            new Datatable(dataTable); // NOSONAR
+        });
+    document.querySelectorAll('[data-table="dataTableDefinitionsBrowser"]')
+        .forEach(dataTable => {
+            // Instantiate a datatable (ignore sonar warnings)
             // eslint-disable-next-line no-new
             new Datatable(dataTable, {
                 pageLength: 300,
-                lengthMenu: [10, 25, 50, 100, 300]
+                lengthMenu: [10, 25, 50, 100, 300],
+                columns: [
+                    null,
+                    null,
+                    {
+                        // Tweak the "rendering" of the 3rd columns:
+                        // Only return the <ol> > <li> text (and exclude, for instance, the whole '<div style="display:none">' used to show details of a node type definition)
+                        render: function (data, type) {
+                            if (type === 'filter') {
+                                // Parse HTML and extract <ol> > <li> text
+                                const temp = document.createElement('div');
+                                temp.innerHTML = data;
+                                const items = temp.querySelectorAll('ol > li');
+                                return Array.from(items).map(li => li.textContent.trim()).join(' ');
+                            }
+
+                            return data;
+                        }
+                    }
+                ]
             }); // NOSONAR
         });
 });
